b75f5948e9d7ae03a1fe6e7b87cbbf8ff1a226e0
LeonRemy <leonremyy2308@gmail.com>
2026-06-30 19:15:47 +0700

    Thêm hàm tìm cây khung Kruskal

--- BTT17.c
+++ BTT17.c
@@ -90,3 +90,76 @@ void Prim() {
     printf("Tong chi phi lam duong: %d\n", tong_chi_phi);
 }
 
+struct Canh {
+    int u, v, trong_so;
+};
+
+int tim_goc(int parent[], int i) {
+    while (parent[i] != i) {
+        i = parent[i];
+    }
+    return i;
+}
+
+void gop(int parent[], int i, int j) {
+    int goc_i = tim_goc(parent, i);
+    int goc_j = tim_goc(parent, j);
+    parent[goc_i] = goc_j;
+}
+
+void Kruskal() {
+    struct Canh danh_sach_canh[MAX * MAX];
+    int so_canh = 0;
+    int i, j;
+
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (ma_tran_ke[i][j] != 0) {
+                danh_sach_canh[so_canh].u = i;
+                danh_sach_canh[so_canh].v = j;
+                danh_sach_canh[so_canh].trong_so = ma_tran_ke[i][j];
+                so_canh++;
+            }
+        }
+    }
+
+    for (i = 0; i < so_canh - 1; i++) {
+        for (j = 0; j < so_canh - i - 1; j++) {
+            if (danh_sach_canh[j].trong_so > danh_sach_canh[j + 1].trong_so) {
+                struct Canh tam = danh_sach_canh[j];
+                danh_sach_canh[j] = danh_sach_canh[j + 1];
+                danh_sach_canh[j + 1] = tam;
+            }
+        }
+    }
+
+    int parent[MAX];
+    for (i = 0; i < n; i++) {
+        parent[i] = i; 
+    }
+
+    printf("\n--- CAY KHUNG KRUSKAL ---\n");
+    printf("Canh\t\tTrong so\n");
+    int canh_da_chon = 0;
+    int tong_chi_phi = 0;
+
+    for (i = 0; i < so_canh; i++) {
+        if (canh_da_chon == n - 1) break; 
+
+        int u = danh_sach_canh[i].u;
+        int v = danh_sach_canh[i].v;
+        int ts = danh_sach_canh[i].trong_so;
+
+        int goc_u = tim_goc(parent, u);
+        int goc_v = tim_goc(parent, v);
+
+        if (goc_u != goc_v) {
+            printf("%d - %d\t\t%d\n", u, v, ts);
+            tong_chi_phi += ts;
+            gop(parent, goc_u, goc_v);
+            canh_da_chon++;
+        }
+    }
+    printf("Tong chi phi lam duong: %d\n", tong_chi_phi);
+}
+
