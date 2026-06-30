70df6332387800ec56d3b762a59229772eac52e2
LeonRemy <leonremyy2308@gmail.com>
2026-06-30 19:14:49 +0700

    Thêm hàm tìm cây khung PRIM

--- BTT17.c
+++ BTT17.c
@@ -47,3 +47,46 @@ void Dijkstra(int diem_bat_dau) {
     }
 }
 
+void Prim() {
+    int parent[MAX]; 
+    int key[MAX];    
+    int da_duyet[MAX];
+    int i, j, count, min, u;
+
+    for (i = 0; i < n; i++) {
+        key[i] = VO_CUC;
+        da_duyet[i] = 0;
+    }
+
+    key[0] = 0; 
+    parent[0] = -1; 
+
+    for (count = 0; count < n - 1; count++) {
+        min = VO_CUC;
+        for (i = 0; i < n; i++) {
+            if (da_duyet[i] == 0 && key[i] < min) {
+                min = key[i];
+                u = i;
+            }
+        }
+
+        da_duyet[u] = 1;
+
+        for (j = 0; j < n; j++) {
+            if (ma_tran_ke[u][j] != 0 && da_duyet[j] == 0 && ma_tran_ke[u][j] < key[j]) {
+                parent[j] = u;
+                key[j] = ma_tran_ke[u][j];
+            }
+        }
+    }
+
+    printf("\n--- CAY KHUNG PRIM ---\n");
+    printf("Canh\t\tTrong so\n");
+    int tong_chi_phi = 0;
+    for (i = 1; i < n; i++) {
+        printf("%d - %d\t\t%d\n", parent[i], i, ma_tran_ke[i][parent[i]]);
+        tong_chi_phi += ma_tran_ke[i][parent[i]];
+    }
+    printf("Tong chi phi lam duong: %d\n", tong_chi_phi);
+}
+
